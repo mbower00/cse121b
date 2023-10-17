8eb1a5fb752824df51d88560c6deef2354d7b5cf
Mitchell Bower <mitchell@bowermail.net>
2023-10-17 01:14:34 -0600

    remove log

--- typeical/scripts/index.js
+++ typeical/scripts/index.js
@@ -1,5 +1,3 @@
-console.log("JS connected!")
-
 import {modalShadeEl, modalXEl, hideModal, showModal} from "./modal.js"
 import {getAnalysis} from "./analysis.js"
 const loadBtn = document.getElementById("load-btn")
